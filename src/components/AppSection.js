8fc3944408591acf3c3ec087f31877effb42cbba
Seitmagambet Olzhas <sei7magambet@gmail.com>
2020-05-31 15:39:29 +0600

    refactor: extract section title

--- src/components/AppSection.js
+++ src/components/AppSection.js
@@ -5,13 +5,12 @@ export const AppSection = {
     title: String
   },
   render(h, context) {
+    const title = h('h2', {
+      class: 'font-bold text-lg'
+    }, context.props.title);
+
     return h('section', {
       class: `px-4 py-3 rounded bg-white shadow-md`
-    }, [
-      h('h2', {
-        class: 'font-bold text-lg'
-      }, context.props.title),
-      context.children
-    ]);
+    }, [title, context.children]);
   }
 };
