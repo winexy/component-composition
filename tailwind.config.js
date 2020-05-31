fc9273e1cbb3e5c91e7871f683d660614680b78a
Seitmagambet Olzhas <sei7magambet@gmail.com>
2020-05-31 15:27:43 +0600

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    textColor: ['responsive', 'group-hover', 'group-focus', 'focus-within', 'first', 'last', 'odd', 'even', 'hover', 'focus', 'active', 'visited', 'disabled']
+  },
+  plugins: [],
+}
